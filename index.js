8f273e91745a992bfe396821c5c955ea9151baa9
Andrew Stegmaier <andrew.stegmaier@microsoft.com>
2024-10-16 11:52:51 -0400

    Refactor for clarity.

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
 document.getElementById("1-interrupted-page-hide").onclick = async () => {
-  const { removeContainer, detachIframe, iframe } = setupIframe();
+  const { mainContainer, container, iframe } = setupIframe();
 
   // Set up two listeners on the iframe
   iframe.contentWindow.addEventListener("pagehide", () => {
     console.log("event listener 1 fired");
-    detachIframe(); // Modifying the DOM in the first listener will cause the second listener to not fire.
+    container.removeChild(iframe); // Modifying the DOM in the first listener will cause the second listener to not fire.
   });
   iframe.contentWindow.addEventListener("pagehide", () => {
     console.log("event listener 2 fired");
   });
 
-  removeContainer();
+  mainContainer.removeChild(container);
 };
 
 function setupIframe() {
@@ -22,9 +22,5 @@ function setupIframe() {
   const iframe = document.createElement("iframe");
   container.appendChild(iframe);
 
-  return {
-    removeContainer: () => mainContainer.removeChild(container),
-    detachIframe: () => container.removeChild(iframe),
-    iframe
-  };
+  return { mainContainer, container, iframe };
 }
