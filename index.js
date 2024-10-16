2ca4820253f0e6f66ef2333f07790444789045c4
Andrew Stegmaier <andrew.stegmaier@microsoft.com>
2024-10-16 12:10:34 -0400

    Remove unnecessary async

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-document.getElementById("1-normal-page-hide").onclick = async () => {
+document.getElementById("1-normal-page-hide").onclick = () => {
   runScenario("pagehide", () => () => {
     console.log("event listener 1 fired");
     // In a normal case, both listeners will fire.
@@ -6,7 +6,7 @@ document.getElementById("1-normal-page-hide").onclick = async () => {
 };
 
 
-document.getElementById("2-interrupted-page-hide").onclick = async () => {
+document.getElementById("2-interrupted-page-hide").onclick = () => {
   runScenario("pagehide", ({ container, iframe }) => () => {
     console.log("event listener 1 fired");
     // Modifying the DOM in the first listener will cause the second listener to not fire in chromium and edge.
