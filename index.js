124ef70983a7ac599c0d446c58156e0de616d6db
Andrew Stegmaier <andrew.stegmaier@microsoft.com>
2024-10-16 11:50:45 -0400

    Set up basic repro.

--- index.js
+++ index.js
@@ -1,4 +1,30 @@
 document.getElementById("1-interrupted-page-hide").onclick = async () => {
-  // TODO: implement this
-  console.log("Hello World!");
+  const { removeContainer, detachIframe, iframe } = setupIframe();
+
+  // Set up two listeners on the iframe
+  iframe.contentWindow.addEventListener("pagehide", () => {
+    console.log("event listener 1 fired");
+    detachIframe(); // Modifying the DOM in the first listener will cause the second listener to not fire.
+  });
+  iframe.contentWindow.addEventListener("pagehide", () => {
+    console.log("event listener 2 fired");
+  });
+
+  removeContainer();
 };
+
+function setupIframe() {
+  const mainContainer = document.getElementById("container");
+
+  const container = document.createElement("div");
+  mainContainer.appendChild(container);
+
+  const iframe = document.createElement("iframe");
+  container.appendChild(iframe);
+
+  return {
+    removeContainer: () => mainContainer.removeChild(container),
+    detachIframe: () => container.removeChild(iframe),
+    iframe
+  };
+}
